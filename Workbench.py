46b4ed2bd313d9889cf2c74d582918034673d4cb
NotSebbbb <sldonahue2201@student.stcc.edu>
2025-01-30 17:39:20 -0500

    Add files via upload

---
+++ Workbench.py
@@ -0,0 +1,22 @@
+#Name: Sebastian Donahue
+#Date: 1/30/25
+#Course Number: 113
+#Course Name: Intro to python
+#Problem Number: Project 1
+#Email: sldonahue@student.stcc.edu
+#Problem Description: Print name as banner
+
+
+
+
+
+
+
+
+print(" SSSSSS EEEEEEEE BBBBBBBB   BBBBBBBB   Y     Y     DDDDD     ")
+print("S       E        B       B  B       B   Y   Y      D    D    ")
+print("S       E        B       B  B       B    Y Y       D     D   ")
+print(" SSSSS  EEEEEEEE BBBBBBBB   BBBBBBBB      Y        D     D   ")
+print("      S E        B        B B        B    Y        D     D   ")
+print("      S E        B        B B        B    Y        D    D  ..") 
+print("SSSSSSS EEEEEEEE BBBBBBBBB  BBBBBBBBB     Y        DDDDD   ..")
